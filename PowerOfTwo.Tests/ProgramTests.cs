d2181bb848eff8941abfbf75d068e35dbdd3c1e5
Михаил <@DESKTOP-V6K7HMK>
2023-04-18 22:06:38 +0300

    Добавьте файлы проекта.

---
+++ PowerOfTwo.Tests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerOfTwo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfTwo.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CheckPowerOfTwoTest()
+        {
+            int x = 32;
+            bool expected = true;
+
+            bool actual = PowerOfTwo.CheckPowerOfTwo(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PowerOfTwo/PowerOfTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfTwo
+{
+    public class PowerOfTwo
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber;
+            bool isPowerOfTwo;
+
+            Console.WriteLine("Введите число:");
+            inputNumber = Convert.ToInt32(Console.ReadLine());
+            isPowerOfTwo = CheckPowerOfTwo(inputNumber);
+            if (isPowerOfTwo)
+                Console.WriteLine($"число {inputNumber} это степень двойки");
+            else
+                Console.WriteLine($"число {inputNumber} это не степень двойки");
+            Console.ReadKey();
+        }
+        public static bool CheckPowerOfTwo(int n)
+        {
+            return n != 0 && (n & (n - 1)) == 0;
+        }
+    }
+}
